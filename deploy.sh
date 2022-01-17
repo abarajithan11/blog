a85e39fec88d5afdc93b487279eeb9b4436185cb
Aba <abarajithan07@gmail.com>
2022-01-18 04:22:55 +0530

    Add new Aussie posts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
-wget -r -nH -P ./docs -E -T 2 -np -k http://localhost:2368/
-echo "----- wget done. Replacing localhost -------"
-find ./docs -type f -exec sed -i 's/http:\/\/localhost:2368/https:\/\/aba-blog.xyz/g' {} \;
-echo "----- Replaced localhost. Replacing og:image -------"
-find ./docs -type f -exec sed -i 's/content="https:\/\/aba-blog.xyz\/content\/images/content="https:\/\/aba-blog.xyz\/content\/images\/size\/w2000/g' {} \;
+wget -r -q -nH -P ./docs -E -T 2 -np -k http://localhost:2368/
+echo "----- wget done. Replacing localhost & og:image -------"
+find ./docs -type f -not -name "*.jpg" -exec sed -i 's/http:\/\/localhost:2368/https:\/\/aba-blog.xyz/g' {} \; -exec sed -i 's/content="https:\/\/aba-blog.xyz\/content\/images/content="https:\/\/aba-blog.xyz\/content\/images\/size\/w2000/g' {} \;
 echo "----- Replaced og:image. Git Add starting -------"
 git add .
 echo "----- Git Add Done -------"
