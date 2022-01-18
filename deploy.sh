36bbf09d7c6a8157be04c492b1a89716ec06ccef
Aba <abarajithan07@gmail.com>
2022-01-18 12:54:49 +0530

    Add Singapore; Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 wget -r -q -nH -P ./docs -E -T 2 -np -k http://localhost:2368/
-echo "----- wget done. Replacing localhost & og:image -------"
-find ./docs -type f -not -name "*.jpg" -exec sed -i 's/http:\/\/localhost:2368/https:\/\/aba-blog.xyz/g' {} \; -exec sed -i 's/content="https:\/\/aba-blog.xyz\/content\/images/content="https:\/\/aba-blog.xyz\/content\/images\/size\/w2000/g' {} \;
+echo "----- wget done. Replacing og:image & localhost -------"
+
+find ./docs -type f -name "*.html" -exec sed -i 's/content="http:\/\/localhost:2368\/content\/images/content="https:\/\/aba-blog.xyz\/content\/images\/size\/w2000/g' {} \; -exec sed -i 's/http:\/\/localhost:2368/https:\/\/aba-blog.xyz/g' {} \; 
+sed -i 's/http:\/\/localhost:2368/https:\/\/aba-blog.xyz/g' docs/robots.txt
+
 echo "----- Replaced og:image. Git Add starting -------"
 git add .
 echo "----- Git Add Done -------"
