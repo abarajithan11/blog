0f614103adfa88a431cb9ae0c49b9c597067f25e
Aba <abarajithan07@gmail.com>
2022-02-08 11:00:26 +0530

    Update deploy with commit & push

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 wget -r -q -nH -P ./docs -E -T 2 -np -k http://localhost:2368/
-echo "----- wget done. Replacing og:image & localhost -------"
+echo "----- wget done. Replacing og:image & localhost... -------"
 
 find ./docs -type f -name "*.html" -exec sed -i 's/content="http:\/\/localhost:2368\/content\/images/content="https:\/\/aba-blog.xyz\/content\/images\/size\/w2000/g' {} \; -exec sed -i 's/http:\/\/localhost:2368/https:\/\/aba-blog.xyz/g' {} \; 
 sed -i 's/http:\/\/localhost:2368/https:\/\/aba-blog.xyz/g' docs/robots.txt
 
-echo "----- Replaced og:image. Git Add starting -------"
+echo "----- Replaced og:image. Git Adding... -------"
 git add .
-echo "----- Git Add Done -------"
+
+if [ "$1" != "no-commit" ]; then
+	echo "----- Git Add Done. Committing... -------"
+	git commit -m "$1"
+	git push
+fi
